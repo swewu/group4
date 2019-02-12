d1080bc91348e3dd5f7c8d6061ddf96623d76dea
godzilowen <owen1491@gmail.com>
2019-02-12 12:32:24 +0700

    fix syntax error Controller Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,9 +1,7 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login extends CI_Controller 
+class Login extends CI_Controller
 {
-
 	public function index()
 	{
 		$this->load->view('login');
