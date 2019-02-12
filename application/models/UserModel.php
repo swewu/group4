4f333b01e6005435033a73b055fa5111e9be6c71
godzilowen <owen1491@gmail.com>
2019-02-12 13:23:58 +0700

    FIX AFTER foreach

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -27,7 +27,7 @@ class UserModel extends CI_Model
         $this->db->where('password', $password);
         $query = $this->db->get('user');
         $_SESSION['login'] = false;
-        foreach($query->result() as $row)
+        foreach ($query->result() as $row) 
         {
             $_SESSION['username'] = $row->username;
             $_SESSION['password'] = $row->password;
