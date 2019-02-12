741a9ff90c5289689fb4ce4ed45d2e50bef61645
godzilowen <owen1491@gmail.com>
2019-02-12 12:03:06 +0700

    fix  syntax error index.php

--- application/views/index.php
+++ application/views/index.php
@@ -18,7 +18,8 @@
     <div class="row col-md-6 col-md-offset-2 custyle">
     <table class="table table-striped custab">
     <thead>
-    <a href="http://localhost/group4/index.php/Subject/add" class="btn btn-primary btn-xs pull-right"><b>+</b> เพิ่มรายวิชา</a>
+    <a href="http://localhost/group4/index.php/Subject/add" 
+    class="btn btn-primary btn-xs pull-right"><b>+</b> เพิ่มรายวิชา</a>
         <tr>
             <!-- <th>รหัสนักศึกษา</th> -->
             <!-- <th>ลำดับ</th> -->
@@ -27,30 +28,42 @@
             <th class="text-center">แก้ไขข้อมูล</th>
         </tr>
     </thead>
-    <?php foreach($result as $row) { ?>
+    <?php foreach ($result as $row) { ?>
         <?php if ($row->status == "W") { ?>
         <tr>
             <td><?=$row->courseName ?></td>
             <td><?=$row->status?></td>
-            <td class="text-center"><a class='btn btn-info btn-xs' href="#"><span class="glyphicon glyphicon-edit"></span> Edit</a> <a href="#" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span> Del</a></td>
+            <td class="text-center"><a class='btn btn-info btn-xs' href="#">
+            <span class="glyphicon glyphicon-edit"></span> Edit</a> 
+            <a href="#" class="btn btn-danger btn-xs">
+            <span class="glyphicon glyphicon-remove"></span> Del</a></td>
         </tr>
-        <?php }else if ($row->status == "F") { ?>
+        <?php } else if ($row->status == "F") { ?>
         <tr>
             <td><?=$row->courseName ?></td>
             <td><?=$row->status?></td>
-            <td class="text-center"><a class='btn btn-info btn-xs' href="#"><span class="glyphicon glyphicon-edit"></span> Edit</a> <a href="#" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span> Del</a></td>
+            <td class="text-center"><a class='btn btn-info btn-xs' href="#">
+            <span class="glyphicon glyphicon-edit"></span> Edit</a> 
+            <a href="#" class="btn btn-danger btn-xs">
+            <span class="glyphicon glyphicon-remove"></span> Del</a></td>
         </tr>
-        <?php }else if ($row->status == "D") { ?>
+        <?php } else if ($row->status == "D") { ?>
         <tr>
             <td><?=$row->courseName ?></td>
             <td><?=$row->status?></td>
-            <td class="text-center"><a class='btn btn-info btn-xs' href="#"><span class="glyphicon glyphicon-edit"></span> Edit</a> <a href="#" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span> Del</a></td>
+            <td class="text-center"><a class='btn btn-info btn-xs' href="#">
+            <span class="glyphicon glyphicon-edit"></span> Edit</a> 
+            <a href="#" class="btn btn-danger btn-xs">
+            <span class="glyphicon glyphicon-remove"></span> Del</a></td>
         </tr>
-        <?php }else { ?>
+        <?php } else { ?>
         <tr>
             <td><?=$row->courseName ?></td>
             <td><?=$row->status?></td>
-            <td class="text-center"><a class='btn btn-info btn-xs' href="#"><span class="glyphicon glyphicon-edit"></span> Edit</a> <a href="#" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span> Del</a></td>
+            <td class="text-center"><a class='btn btn-info btn-xs' href="#">
+            <span class="glyphicon glyphicon-edit"></span> Edit</a> 
+            <a href="#" class="btn btn-danger btn-xs">
+            <span class="glyphicon glyphicon-remove"></span> Del</a></td>
         </tr>
         <?php } ?>
     <?php } ?>
